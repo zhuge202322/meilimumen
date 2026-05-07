d66490ebe3ad289bf7a57a05ed15d29f31f80d78
KIDSRIDE Developer <zhuge202322@gmail.com>
2026-05-07 01:50:33 -0400

    fix: allow remote images from WP server in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '45.145.229.20',
+        port: '2656',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
         source: '/api/wp/:path*',
-        destination: 'http://45.145.229.20:2656/:path*',
-      },
-    ];
-  },
+        destination: 'http://45.145.229.20:2656/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
